c8eabccae5b7db487b472e3aa8ada2b192f8c3fa
Aryanshlakrachauhan <ARYANSH.29453@stu.upes.ac.in>
2025-11-14 15:28:39 +0530

    Add files via upload

---
+++ Day 74/Q124.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main(){
+
+  FILE *fr;
+  FILE *fw;
+  fr = fopen("source.txt", "r");
+  fw = fopen("destination.txt", "w");
+
+  char ch;
+  char source[100], destination[100];
+  while ((ch = fgetc(fr)) != EOF){
+    fputc(ch, fw);
+  }
+  
+  printf("File copied to destination.txt");
+  fclose(fr);
+  fclose(fw);
+  
+}
